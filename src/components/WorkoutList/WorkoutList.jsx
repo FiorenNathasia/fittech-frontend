78ab20e0dcbcc75f67c720690e5399486474534f
FiorenNathasia <fiorennathasia@gmail.com>
2025-05-29 23:45:29 +0100

    Remove maxwidth

--- src/components/WorkoutList/WorkoutList.jsx
+++ src/components/WorkoutList/WorkoutList.jsx
@@ -7,7 +7,6 @@ function WorkoutList({ workouts, fetchWorkouts }) {
     <Box
       sx={{
         width: "100%",
-        maxWidth: { xs: "22rem", sm: none },
         margin: "auto",
         marginTop: { xs: "100px", sm: "30px" },
         marginBottom: { xs: "110px", sm: "0px" },
